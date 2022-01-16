f54e9d31d7a87d9a51d67ce8ffa67b8bf928f578
ian_otieno <ian.otieno@student.moringaschol.com>
2022-01-16 13:18:46 +0300

    add get pizza function

--- script.js
+++ script.js
@@ -2,5 +2,14 @@
 var price, crust_price, topping_price;
 let total = 0;
 
+// Getpizza Function
+function Getpizza(name, size, crust, topping, total) {
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.total = total;
+}
+
 
 });
